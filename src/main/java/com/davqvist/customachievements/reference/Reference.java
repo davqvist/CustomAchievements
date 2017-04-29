aea6941502987b70e46795184b2500f043fec09b
Vasco Lavos <vazkii@hotmail.com>
2017-04-29 21:44:58 +0100

    Maintain version scheme

--- src/main/java/com/davqvist/customachievements/reference/Reference.java
+++ src/main/java/com/davqvist/customachievements/reference/Reference.java
@@ -3,7 +3,7 @@ package com.davqvist.customachievements.reference;
 public class Reference {
     public static final String MOD_ID = "customachievements";
     public static final String MOD_NAME = "Custom Achievements";
-    public static final String VERSION = "1.11.2-0.3";
+    public static final String VERSION = "1.11.2-0.3.0";
     public static final String CLIENT_PROXY_CLASS = "com.davqvist.customachievements.proxy.ClientProxy";
     public static final String SERVER_PROXY_CLASS = "com.davqvist.customachievements.proxy.ServerProxy";
 }
