d85b36684ff43e6f0a8a567b757af29aa6db67c7
Vasco Lavos <vazkii@hotmail.com>
2017-04-29 23:55:28 +0100

    ALL THE CHANGES
    Here's all that was done:
    - Major code refractor and rewrite on the achievement handling parts of the mod
    - Multiple achievement tabs are now allowed
    - New triggers: Kill mob, place block, mine block, stat comparison
    - Demos for multiple tabs and the new triggers
    - Fixed the trophy model going out of bounds in the GUI by removing the needless transformations

--- src/main/java/com/davqvist/customachievements/Achievements.java
+++ src/main/java/com/davqvist/customachievements/Achievements.java
@@ -1,76 +1,71 @@
 package com.davqvist.customachievements;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 import com.davqvist.customachievements.config.AchievementsReader;
 import com.davqvist.customachievements.config.AchievementsReader.AchievementDesciptor;
 import com.davqvist.customachievements.config.AchievementsReader.AchievementTab;
+import com.davqvist.customachievements.config.AchievementsReader.AchievementType;
 import com.davqvist.customachievements.init.ModBlocks;
 import com.davqvist.customachievements.utility.NBTHelper;
+import com.google.common.collect.BiMap;
+import com.google.common.collect.HashBiMap;
+import com.google.common.collect.HashMultimap;
+import com.google.common.collect.Multimap;
 
 import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.stats.Achievement;
+import net.minecraft.stats.StatList;
 import net.minecraftforge.common.AchievementPage;
 
 public class Achievements {
-    public static AchievementPage page;
 
-    public static Map<String, Achievement> achievements = new HashMap<String, Achievement>();
-    public static Map<Integer, Boolean> achievementsIgnoreMeta = new HashMap<Integer, Boolean>();
-    public static Map<Integer, Boolean> achievementsTrophy = new HashMap<Integer, Boolean>();
-    public static List<Achievement> detectAchievements = new ArrayList<>();
-    public static List<Achievement> craftAchievements = new ArrayList<>();
+    public static BiMap<AchievementDesciptor, Achievement> achievementsDescriptors = HashBiMap.<AchievementDesciptor, Achievement>create();
+    public static Multimap<AchievementType, AchievementDesciptor> achievementHandlers = HashMultimap.<AchievementType, AchievementDesciptor>create();
 
     public static void init() {
-    	for(AchievementTab tab : CustomAchievements.proxy.ar.root.tabs)
-	        for( AchievementsReader.AchievementDesciptor alist : tab.achievements ){
-	            if( !alist.uid.isEmpty() ){
+    	for(AchievementTab tab : CustomAchievements.proxy.ar.root.tabs) {
+        	List<Achievement> tabAchievements = new ArrayList();
+
+	        for(AchievementsReader.AchievementDesciptor desc : tab.achievements){
+	            if(!desc.uid.isEmpty()){
 	                int meta = 0;
-	                if( alist.meta != null ){ meta = alist.meta; }
-	                ItemStack is = new ItemStack( Item.getByNameOrId( alist.item ), 1, meta );
-	                if( !is.isEmpty() ) {
-	                    Achievement tempAch = new Achievement( "achievement." + alist.uid, alist.uid, alist.xpos, alist.ypos, is, achievements.get( alist.parent ) ).registerStat();
-	                    achievements.put( alist.uid, tempAch );
-	                    achievementsIgnoreMeta.put( tempAch.hashCode(), alist.ignoreMeta );
-	                    achievementsTrophy.put( tempAch.hashCode(), alist.trophy );
-	
-	                    if( alist.type.equals( "Detect" ) ){ detectAchievements.add( tempAch ); }
-	                    if( alist.type.equals( "Craft" ) ){ craftAchievements.add( tempAch ); }
-	                }
+	                if(desc.meta != null)
+	                	meta = desc.meta;
+	                
+	                ItemStack is = new ItemStack(Item.getByNameOrId(desc.item), 1, meta);
+	                Achievement parent = null;
+	                if(desc.parent != null && !desc.parent.isEmpty())
+	                	parent = (Achievement) StatList.getOneShotStat("achievement." + desc.parent);
+	                
+                    Achievement tempAch = new Achievement("achievement." + desc.uid, desc.uid, desc.xpos, desc.ypos, is, parent).registerStat();
+                    achievementsDescriptors.put(desc, tempAch);
+                    achievementHandlers.put(desc.type, desc);
+                    tabAchievements.add(tempAch);
 	            }
 	        }
-
-        for(AchievementTab tab : CustomAchievements.proxy.ar.root.tabs) {
-	        String tabName = tab.tabname.isEmpty() ? "Custom Achievements" : tab.tabname;
-	        List<Achievement> achievementList = tab.achievements.stream().filter((a) -> achievements.containsKey(a.uid)).map((a) -> achievements.get(a.uid)).collect(Collectors.toList());
-	        page = new AchievementPage( tabName, achievementList.toArray( new Achievement[achievementList.size()] ) );
-	        AchievementPage.registerAchievementPage( page );
-        }
+	        
+	        String tabName = tab.tabname.isEmpty() ? "NO NAME!" : tab.tabname;
+	        AchievementPage page = new AchievementPage(tabName, tabAchievements.toArray(new Achievement[tabAchievements.size()]));
+	        AchievementPage.registerAchievementPage(page);
+    	}
     }
 
     public static void trigger( Achievement achievement, EntityPlayer player ){
-        if( player != null ){
-            boolean getTrophy = false;
-            if( !player.world.isRemote && player instanceof EntityPlayerMP && !player.hasAchievement( achievement ) && achievementsTrophy.get( achievement.hashCode() ) ){
-                getTrophy = true;
-            }
-            player.addStat( achievement );
-            if( player.hasAchievement( achievement ) && getTrophy ){
-                ItemStack is = new ItemStack( ModBlocks.trophy );
-                NBTTagCompound compound = NBTHelper.getTagCompound( is );
-                compound.setTag( "item", achievement.theItemStack.serializeNBT() );
-                compound.setString( "player", player.getName() );
-                is.setTagCompound( compound );
-                player.world.spawnEntity( new EntityItem( player.world, player.posX, player.posY, player.posZ, is ) );
+        if(player != null && !player.world.isRemote && !player.hasAchievement(achievement)){
+            player.addStat(achievement);
+            if(achievementsDescriptors.inverse().get(achievement).trophy){
+                ItemStack is = new ItemStack(ModBlocks.trophy);
+                NBTTagCompound compound = NBTHelper.getTagCompound(is);
+                compound.setTag("item", achievement.theItemStack.serializeNBT());
+                compound.setString("player", player.getName());
+                is.setTagCompound(compound);
+                player.world.spawnEntity(new EntityItem(player.world, player.posX, player.posY, player.posZ, is));
             }
         }
     }
